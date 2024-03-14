657216cd928e6ab851a0a955fd1a5ae942d8daf5
akash <akash.ramolia@gmail.com>
2024-03-14 22:28:35 +0530

    set example

---
+++ language/sets.js
@@ -0,0 +1,20 @@
+/* 
+
+Question : you are given a string, for example "akash", you have to return the number of unique charactors you see
+in the string. Here ans is 4, becausethere are 4 unique charactors: a k s h.
+
+Solution: Whenever there is a mention of uniqueness or removal of duplicates,
+sets is used. I will put all charactors inside the set and will return the size of the sets.
+
+
+ */
+
+finduniquecharactors(){
+    let str;
+
+    let set = new Set();
+    for(let char in str){
+        set.push();
+    }
+    
+}
